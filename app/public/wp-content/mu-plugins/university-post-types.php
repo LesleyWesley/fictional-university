fcf76419d5b7302887b2ddb7f276146f8e4c4b49
lesley_w <lrwesley92@gmail.com>
2024-06-09 14:54:21 -0500

    Added Program custom post type and pages for program singles and archives

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
 <?php
     function university_post_types() {
+      //Event Post Type
       register_post_type("event", array(
         "show_in_rest" => true,
         "supports" => array("title", "editor", "excerpt"),
@@ -13,6 +14,23 @@
           "edit_item" => "Edit Event",
           "all_items" => "All Events",
           "singular_name" => "Event"
+        )
+      ));
+
+      //Program Post type
+      register_post_type("program", array(
+        "show_in_rest" => true,
+        "supports" => array("title", "editor"),
+        "has_archive" => true,
+        "rewrite" => array("slug" => "programs"),
+        "public" => true,
+        "menu_icon" => "dashicons-awards",
+        "labels" => array(
+          "name" => "Programs",
+          "add_new_item" => "Add New Program",
+          "edit_item" => "Edit Program",
+          "all_items" => "All Programs",
+          "singular_name" => "Program"
         )
       ));
     }
